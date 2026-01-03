cec1d4441082b1e25ee0ee12c93a723ffe0e5b80
Karlos <batistaleitecardozokarlos@gmail.com>
2026-01-02 20:35:15 -0400

    de karlos a main

--- src/components/shared/Home/HomeSobrenosotros.jsx
+++ src/components/shared/Home/HomeSobrenosotros.jsx
@@ -3,12 +3,32 @@ import './HomeSobrenosotros.css'
 
 function HomeSobrenosotros() {
   return (
-    <div>
-        <div className='negro'></div>
-        <div className='celeste'></div>
-        <div className='blanco'></div>
-      
-    </div>
+    <>
+    <section className='nosotros-section'>
+        <div className='nosotros-conteainer'>
+        <div className='nosotros-history'>
+            <span className='nosotros-inicio'>Desde 2010</span>
+            <h1 className='nosotros-tittle'>MADE Desarrolladora</h1>
+            <div className='dividor'></div>
+            <p className='nosotros-description'>                
+                Somos especialistas en la creación de proyectos inmobiliarios integrales. 
+                Desde 2010, hemos transformado terrenos en comunidades vibrantes y espacios 
+                en hogares excepcionales.
+            </p>
+            <p className="description-description">
+                Nuestro enfoque combina visión de mercado, diseño innovador y ejecución 
+                impecable para crear valor duradero para nuestros clientes y las comunidades 
+                donde operamos.
+            </p>
+            <button>Conose nuestra historia</button>
+        </div>
+
+        <div className='nosotros-stats'>
+            <img src="" alt="nosotros-img"/>
+        </div>
+        </div>
+    </section>
+    </>
   )
 }
 
