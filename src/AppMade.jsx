9843d554047c63c70213c3c05f81cd83ccb77994
Karlos <batistaleitecardozokarlos@gmail.com>
2026-02-21 22:42:48 -0400

    estructura inicial

--- src/AppMade.jsx
+++ src/AppMade.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './layout/Navbar/Navbar';
+import ROUTES  from './shared/constants/routes';
+// import HomePage from './pages/HomePage';
+// import AboutPage from './pages/AboutPage';
+// import ProjectsPage from './pages/ProjectsPage';
+// import ContactPage from './pages/ContactPage';
+// import LoginPage from './pages/LoginPage';
 
-export default function AppMade() {
+function App() {
   return (
-    <div className='bg-red-400'>AppMade</div>
-  )
+    <Router>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          {/* <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.ABOUT_US} element={<AboutPage />} />
+          <Route path={ROUTES.PROJECTS} element={<ProjectsPage />} />
+          <Route path={ROUTES.CONTACT} element={<ContactPage />} />
+          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+          <Route path="*" element={<div>404 - Page Not Found</div>} /> */}
+        </Routes>
+      </main>
+    </Router>
+  );
 }
+
+export default App;
---
+++ src/shared/constants/routes.js
@@ -0,0 +1,9 @@
+const ROUTES = {
+    HOME: '/',
+    ABOUT_US: '/about',
+    PROJECTS: '/projects',
+    CONTACT: '/contact',
+    LOGIN: '/login'
+}
+
+export default ROUTES
