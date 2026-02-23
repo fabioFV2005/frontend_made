e0bab254e049d9106c7627068347e66176bbe1b5
matias_santiago <santiagomatriverop@gmail.com>
2026-02-23 02:45:20 -0400

    SCRUM13 AVANCE

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeHero from '../shared/components/Home/HomeHero'
+import Hero2 from '../shared/components/Home/Hero2'
 
 function Home() {
   return (
     <div>
         <HomeHero/>
+        <Hero2/>
     </div>
   )
 }
