79e4003a97fa1e9ba8156eff591e58d73e2c7f28
Karlos <batistaleitecardozokarlos@gmail.com>
2026-02-22 20:05:07 -0400

    SCRUM-11/ definicion de variables

--- src/shared/components/custom/CustomHeader.jsx
+++ src/shared/components/custom/CustomHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Logo from '../../../assets/Made/MadeLogo.png'
+import Logo from '../../../assets/Made/MadeLogo2.png'
 export const CustomHeader = () => {
     const [isScrolled, setIsScrolled] = useState(false)
     useEffect(() => {
@@ -13,9 +13,9 @@ export const CustomHeader = () => {
 
     return (
 
-        <header className='flex items-center justify-between p-4 bg-transparent text-white'>
-            <img src={Logo} alt="Logo" className='w-60' />
-            <ul className='flex flex-col md:flex-row gap-4 text-lg'>
+        <header className='flex items-center justify-between p-4 bg-transparent text-(--text-light)'>
+            <img src={Logo} alt="Logo" className='w-50' />
+            <ul className='flex flex-col md:flex-row gap-3 text-lg'>
                 <li>
                     <a href="">Inicio</a>
                 </li>
@@ -29,7 +29,7 @@ export const CustomHeader = () => {
                     <a href="">Contacto</a>
                 </li>
                 <li>
-                    <a href="" className='bg-orange-500 text-white px-4 py-2 rounded'>Iniciar Sesion</a>
+                    <a href="" className='bg-(--cl-orange) text-(--text-light) px-4 py-2 rounded'>Iniciar Sesion</a>
                 </li>
             </ul>
         </header>
